bc0d727afc3a690ec7288c591fc8a9e80f540f53
M-Aranda <cheloz_20@hotmail.com>
2018-07-17 14:27:55 -0300

    terminada 2nda entrega, pero puede que los triggers esten mas o

--- scriptExamenBD.sql
+++ scriptExamenBD.sql
@@ -160,11 +160,11 @@ INSERT INTO registro (patente, rut, fecha_cesion) VALUES ('FKUI33','10',GETDATE(
 INSERT INTO registro (patente, rut, fecha_cesion)  VALUES ('FKUI44','70',GETDATE())
 GO
 
-INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(200,200,3000000)
-INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(202,202,3000000)
-INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(204,204,3000000)
-INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(206,206,3000000)
-INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(208,208,3000000)
+INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(200,200,300000)
+INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(202,202,100000)
+INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(204,204,300000)
+INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(206,206,260000)
+INSERT INTO complemento (codigo_vehiculo, codigo_opcion, precio) VALUES(208,208,300000)
 GO
 
 INSERT INTO venta (rut_cliente,rut_vendedor, codigo_vehiculo, fecha) VALUES ('20','22',200,GETDATE())
@@ -185,10 +185,168 @@ venta, vehiculo_nuevo WHERE registro.patente=vehiculo_usado.patenteU AND registr
 venta.rut_cliente=cliente.rutC AND venta.codigo_vehiculo=vehiculo_nuevo.codigo
 GO
 
-/*
-SELECT * FROM View_Cliente_Usado
+CREATE PROCEDURE proc_factura (@codigoV INT, @rutDelCliente INT) AS -- DROP PROCEDURE proc_factura
+	BEGIN
+	DECLARE @precioSinIva INT
+	DECLARE @precioConIva INT
+	DECLARE @precioComplemento INT
+	DECLARE @precioFinal INT
+
+	SET @precioSinIva=(SELECT precio FROM vehiculo_nuevo WHERE codigo=@codigoV)
+	SET @precioConIva=@precioSinIva+((@precioSinIva*19)/100)
+	SET @precioComplemento=(SELECT complemento.precio FROM complemento WHERE complemento.codigo_vehiculo=@codigoV)
+	SET @precioFinal=@precioConIva+@precioComplemento
+
+	SELECT cliente.rutC AS 'Rut Cliente', cliente.nombreC AS 'Nombre cliente', cliente.direccion AS 'Direccion',
+	cliente.telefono AS 'Telefono', venta.fecha AS 'Fecha de venta', vendedor.nombre AS 'Vendedor',
+	vehiculo_nuevo.codigo AS 'Codigo vehiculo',vehiculo_nuevo.marca AS 'Marca', vehiculo_nuevo.modelo AS 'Modelo',
+	vehiculo_nuevo.cilindrada AS 'Cilindrada', vehiculo_nuevo.precio AS 'Precio del vehiculo',
+	opcion.nombre AS 'Opcion', @precioComplemento AS 'Precio Complemento', @precioFinal AS 'Precio Final' FROM cliente, vendedor, complemento, vehiculo_nuevo, venta, opcion
+	WHERE cliente.rutC=venta.rut_cliente AND vendedor.rut=venta.rut_vendedor AND vehiculo_nuevo.codigo=complemento.codigo_vehiculo
+	AND complemento.codigo_opcion=opcion.codigo AND vehiculo_nuevo.codigo=@codigoV AND cliente.rutC=@rutDelCliente
+
+	END
 GO
-*/
+
+-- EXEC proc_factura 200,'20'
+
+
+CREATE FUNCTION [dbo].[aumentarPrecioModelo](@mode VARCHAR(20), @porc INT)-- DROP FUNCTION aumentarPrecioModelo
+RETURNS INT
+AS
+BEGIN
+DECLARE @sql varchar(4000), @cmd varchar(4000), @num INT, @aumento INT,
+ @nuevoPrecioDeVehiculo INT, @precioDeEsteVehiculo INT, @codigoV INT 
+DECLARE aumentaPrecios CURSOR
+
+		FOR(SELECT precio, codigo FROM vehiculo_nuevo WHERE modelo=@mode)
+		OPEN aumentaPrecios
+
+		FETCH NEXT FROM aumentaPrecios INTO @precioDeEsteVehiculo, @codigoV
+			SET @aumento=(@precioDeEsteVehiculo*@porc)/100
+			SET @nuevoPrecioDeVehiculo=@precioDeEsteVehiculo+@aumento
+	
+		SET @sql='UPDATE  vehiculo_nuevo SET precio= ' + CONVERT(VARCHAR,@nuevoPrecioDeVehiculo)  + ' WHERE modelo= (''' + @mode + ''') AND codigo=' + CONVERT(VARCHAR,@codigoV)  + ''
+		SELECT @cmd = 'sqlcmd -S ' + @@servername +
+              ' -d ' + db_name() + ' -Q "' + @sql + '"'
+	EXEC master..xp_cmdshell @cmd, 'no_output'
+
+		WHILE @@FETCH_STATUS=0
+		BEGIN
+			FETCH NEXT FROM aumentaPrecios INTO @precioDeEsteVehiculo, @codigoV
+			SET @aumento=(@precioDeEsteVehiculo*@porc)/100
+			SET @nuevoPrecioDeVehiculo=@precioDeEsteVehiculo+@aumento
+			SET @sql='UPDATE  vehiculo_nuevo SET precio= ' + CONVERT(VARCHAR,@nuevoPrecioDeVehiculo)  + ' WHERE modelo= (''' + @mode + ''') AND codigo=' + CONVERT(VARCHAR,@codigoV)  + ''
+SELECT @cmd = 'sqlcmd -S ' + @@servername +
+              ' -d ' + db_name() + ' -Q "' + @sql + '"'
+EXEC master..xp_cmdshell @cmd, 'no_output'
+		END
+
+CLOSE aumentaPrecios
+DEALLOCATE aumentaPrecios
+RETURN 1
+END
+GO
+
+-- SELECT dbo.aumentarPrecioModelo ('Deportivo',10)
+-- SELECT * FROM vehiculo_nuevo
+
+CREATE TABLE VENTAS_EJECUTIVAS (
+id INT IDENTITY (1,1),
+rutVendedor VARCHAR (10),
+nombreVendedor VARCHAR (70),
+valorVehiculo INT,
+comisionRealizada INT,
+FOREIGN KEY (rutVendedor) REFERENCES vendedor(rut),
+PRIMARY KEY (id)
+)
+
+
+CREATE TRIGGER comisionDeVenta ON venta AFTER INSERT, DELETE AS -- DROP TRIGGER comisionDeVenta
+
+DECLARE @idVentaMasReciente INT
+DECLARE @rutVend VARCHAR (10)
+DECLARE @nomVend VARCHAR (70)
+DECLARE @valorVehiculo INT
+DECLARE @comision INT
+
+DECLARE @rowcount int
+SET @rowcount = @@rowcount
+DECLARE @boletaBorrada INT
+SET @boletaBorrada=@rowcount
+
+
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @action as char(1);
+
+    SET @action = 'I'; 
+    IF EXISTS(SELECT * FROM DELETED)
+    BEGIN
+        SET @action = 
+            CASE
+                WHEN EXISTS(SELECT * FROM INSERTED) THEN 'U' 		
+                ELSE 'D'    
+            END
+    END
+    ELSE 
+        IF NOT EXISTS(SELECT * FROM INSERTED) RETURN;
+    END
+
+IF @action='I'
+SET @idVentaMasReciente=(SELECT MAX(id) FROM venta)
+
+SET @rutVend=(SELECT venta.rut_vendedor FROM venta, vendedor WHERE venta.rut_vendedor=vendedor.rut AND venta.id=@idVentaMasReciente)
+SET @nomVend=(SELECT vendedor.nombre FROM venta, vendedor WHERE venta.rut_vendedor=vendedor.rut AND venta.id=@idVentaMasReciente)
+SET @valorVehiculo=(SELECT vehiculo_nuevo.precio FROM venta, vehiculo_nuevo WHERE venta.codigo_vehiculo=vehiculo_nuevo.codigo AND venta.id=@idVentaMasReciente)
+SET @comision=(@valorVehiculo*5)/100
+
+INSERT INTO VENTAS_EJECUTIVAS VALUES (@rutVend, @nomVend, @valorVehiculo, @comision)
+
+IF @action='D'
+DELETE FROM VENTAS_EJECUTIVAS WHERE id=@boletaBorrada
+GO
+
+
+CREATE TABLE EVENTOS(
+ID INT IDENTITY(1,1),
+fechaYHoraDeEvento DATETIME,
+tipoDeEvento VARCHAR(200),
+usuario VARCHAR (50),
+PRIMARY KEY (id)
+)
+GO
+
+
+CREATE TRIGGER registraEventos ON vehiculo_nuevo AFTER INSERT, DELETE, UPDATE
+AS 
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @action as char(1);
+
+    SET @action = 'I'; 
+    IF EXISTS(SELECT * FROM DELETED)
+    BEGIN
+        SET @action = 
+            CASE
+                WHEN EXISTS(SELECT * FROM INSERTED) THEN 'U' 		
+                ELSE 'D'    
+            END
+    END
+    ELSE 
+        IF NOT EXISTS(SELECT * FROM INSERTED) RETURN;
+    END
+
+	IF @action='I' 
+	INSERT INTO EVENTOS VALUES (CURRENT_TIMESTAMP,'Se agregaron datos', 'Administrador')
+	ELSE IF @action='D'
+	INSERT INTO EVENTOS VALUES (CURRENT_TIMESTAMP,'Se borraron datos', 'Administrador')
+	ELSE IF @action='U'
+	INSERT INTO EVENTOS VALUES (CURRENT_TIMESTAMP,'Se actualizaron datos', 'Administrador')
+	GO
+
+
+
 
 USE MASTER
 GO
@@ -196,3 +354,118 @@ ALTER DATABASE examenBDMarcelo_Aranda
 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 DROP DATABASE examenBDMarcelo_Aranda
 GO
+
+/*
+SELECT * FROM View_Cliente_Usado
+GO
+
+
+CREATE TRIGGER borrarComisionVenta ON venta AFTER DELETE AS
+DECLARE @rowcount int
+SET @rowcount = @@rowcount
+DECLARE @boletaBorrada INT
+SET @boletaBorrada=@rowcount
+
+DELETE FROM VENTAS_EJECUTIVAS WHERE id=@boletaBorrada
+GO
+
+
+EXEC sp_configure'xp_cmdshell', 1
+GO
+EXEC sp_configure 'show advanced options', 1
+GO
+RECONFIGURE
+GO
+
+
+/*
+CREATE FUNCTION [dbo].[_tmp_func](@mode VARCHAR(20))
+RETURNS INT
+AS
+BEGIN
+DECLARE @sql varchar(4000), @cmd varchar(4000)
+SELECT @sql = 'UPDATE  vehiculo_nuevo SET precio=1 WHERE modelo= (''' + @mode + ''') '
+SELECT @cmd = 'sqlcmd -S ' + @@servername +
+              ' -d ' + db_name() + ' -Q "' + @sql + '"'
+EXEC master..xp_cmdshell @cmd, 'no_output'
+RETURN 1
+END
+GO
+
+SELECT dbo._tmp_func ('Deportivo')
+*/
+
+
+
+
+
+CREATE FUNCTION [dbo].[_tmp_func](@mode VARCHAR(20))-- DROP FUNCTION _tmp_func
+RETURNS INT
+AS
+BEGIN
+DECLARE @sql varchar(4000), @cmd varchar(4000), @num INT
+SET @num=2
+SET @sql='UPDATE  vehiculo_nuevo SET precio= ' + CONVERT(VARCHAR,@num)  + ' WHERE modelo= (''' + @mode + ''') '
+SELECT @cmd = 'sqlcmd -S ' + @@servername +
+              ' -d ' + db_name() + ' -Q "' + @sql + '"'
+EXEC master..xp_cmdshell @cmd, 'no_output'
+RETURN 1
+END
+GO
+
+SELECT dbo._tmp_func ('Deportivo')
+
+
+
+
+
+
+Este no servia
+CREATE FUNCTION [dbo].[aumentarPrecioModelo] (@porc INT, @modeloAAumentar VARCHAR (20)) RETURNS INT AS -- DROP FUNCTION dbo.aumentarPrecioModelo 
+		
+	BEGIN
+		DECLARE @precioDeEsteVehiculo INT
+		DECLARE @nuevoPrecioDeVehiculo INT
+		DECLARE @aumento INT
+		DECLARE @query VARCHAR(4000)
+		DECLARE @cmd VARCHAR (4000)
+		DECLARE aumentaPrecios CURSOR 
+
+		
+		FOR(SELECT precio FROM vehiculo_nuevo WHERE modelo=@modeloAAumentar)
+		OPEN aumentaPrecios
+
+		FETCH NEXT FROM aumentaPrecios INTO @precioDeEsteVehiculo
+			SET @aumento=(@precioDeEsteVehiculo*@porc)/100
+			SET @nuevoPrecioDeVehiculo=@precioDeEsteVehiculo+@aumento
+
+
+		SET @query='UPDATE  vehiculo_nuevo SET precio= ' + CONVERT(VARCHAR,@nuevoPrecioDeVehiculo)  + ' WHERE modelo= (''' + @query + ''') '
+		SELECT @cmd = 'sqlcmd -S ' + @@servername +
+              ' -d ' + db_name() + ' -Q "' + @query + '"'
+		EXEC master..xp_cmdshell @cmd, 'no_output'	
+
+		WHILE @@FETCH_STATUS=0
+			FETCH NEXT FROM aumentaPrecios INTO @precioDeEsteVehiculo
+			SET @aumento=(@precioDeEsteVehiculo*@porc)/100
+			SET @nuevoPrecioDeVehiculo=@precioDeEsteVehiculo+@aumento
+			
+		SET @query='UPDATE  vehiculo_nuevo SET precio= ' + CONVERT(VARCHAR,@nuevoPrecioDeVehiculo)  + ' WHERE modelo= (''' + @query + ''') '
+		SELECT @cmd = 'sqlcmd -S ' + @@servername +
+              ' -d ' + db_name() + ' -Q "' + @query + '"'
+		EXEC master..xp_cmdshell @cmd, 'no_output'	
+
+		CLOSE aumentaPrecios
+		DEALLOCATE aumentaPrecios
+
+		
+		EXEC sp_configure'xp_cmdshell', 1 
+		RETURN 1
+
+	END
+GO
+
+
+SELECT dbo.aumentarPrecioModelo (10,'Deportivo')
+GO
+*/
